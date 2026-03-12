edef3c4deb5d206f2fda8800647d295bc900b6a5
xerodiningrat <febyanta6@gmail.com>
2026-03-12 21:54:41 +0700

    Update browser tab branding for LYVA STUDIO

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,13 +1,18 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+@php
+    $brandName = 'LYVA STUDIO';
+    $pageTitle = filled($title ?? null) ? $brandName.' | '.$title : $brandName;
+@endphp
+
 <title>
-    {{ filled($title ?? null) ? $title.' - '.config('app.name', 'Laravel') : config('app.name', 'Laravel') }}
+    {{ $pageTitle }}
 </title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="512x512" href="{{ asset('lyva-studio-logo-square.png') }}">
+<link rel="shortcut icon" type="image/png" href="{{ asset('lyva-studio-logo-square.png') }}">
+<link rel="apple-touch-icon" href="{{ asset('lyva-studio-logo-1x1.png') }}">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
